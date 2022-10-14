cf6575482f13495a2eaf6eaa7606e6846c0d3b5d
Alexandru Petrut <alexandru.petrut@exacon.it>
2022-10-15 01:16:53 +0200

    Average

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,5 +1,27 @@
-import 'package:demo/demo.dart' as demo;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${demo.calculate()}!');
+  if (arguments.isEmpty) {
+    print('Help data');
+    exit(0);
+  }
+  String firstArgument = arguments.first;
+  switch (firstArgument) {
+    case 'avg':
+      average(arguments);
+      return;
+    default:
+      print('Help data');
+      return;
+  }
+}
+
+void average(List<String> arguments) {
+  try {
+    final numbers = arguments.sublist(1).map((e) => double.parse(e));
+    final avg = numbers.fold<double>(0, (value, e) => value + e) / numbers.length;
+    print('Average: $avg');
+  } on FormatException {
+    print('Invalid numbers');
+  }
 }
